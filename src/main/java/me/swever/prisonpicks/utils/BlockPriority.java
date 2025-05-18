58df5cf3923c1f09352e09d93da99955b292ae52
AntTheAntster <anttheantster@gmail.com>
2025-05-19 00:56:20 +0100

    Missed a bit of commenting in the BlockPriority.java class

--- src/main/java/me/swever/prisonpicks/utils/BlockPriority.java
+++ src/main/java/me/swever/prisonpicks/utils/BlockPriority.java
@@ -6,6 +6,20 @@ import java.util.Map;
 
 public class BlockPriority {
 
+    /*
+    Class needs initialising in the main class, otherwise the map doesn't yet exist when you try to use it.
+    This is because the map is static, meaning it is a class variable and not an instance variable. (Google the difference between static and instance variables for more info).
+    The class needs a constructor in here too. We have already done constructors in the pickaxe classes, so you can use them as reference.
+    Hint: public BlockPickaxe() {}
+
+    You should also initialise the valueMap in a private void as opposed to declaring the map's contents upon creation of the map.
+    Hint: private void initValueMap() {
+        valueMap.put(Material.EMERALD_BLOCK, 9);
+        valueMap.put(Material.EMERALD_ORE, 8);
+        (etc)
+    }
+    Remember to initialise the map in the class constructor (Again, we have done stuff like this in the pickaxe classes).
+     */
     public static final Map<Material, Integer> valueMap = Map.of(
             Material.EMERALD_BLOCK, 9,
             Material.EMERALD_ORE, 8,
